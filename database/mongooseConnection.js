3e5d6d2728815524197fd330348e9d10d878b5d9
ZhaoqiuD <dingzq0807@gmail.com>
2024-05-24 11:45:40 -0700

    Updated the data and display it in calendar

---
+++ database/mongooseConnection.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+require('./schemas/students');
+require('./schemas/courses');
+require("./schemas/instructors");
+require('./schemas/chat');
+
+require("dotenv").config();
+const mongodb_host = process.env.MONGODB_HOST;
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_password = process.env.MONGODB_PASSWORD;
+mongoose
+    .connect(
+        `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/?retryWrites=true&w=majority&appName=Cluster0`
+    )
+    .then(() => console.log("MongoDB Connected!"));
