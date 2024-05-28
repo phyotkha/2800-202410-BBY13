7366da404aecfc474e18ab1b977d3fef420a477f
ZhaoqiuD <dingzq0807@gmail.com>
2024-05-28 11:25:05 -0700

    added update and delete events

--- database/routers/events.js
+++ database/routers/events.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createEvent } = require("../services/events");
+const { createEvent, updateEvent, deleteEvent } = require("../services/events");
 const router = express.Router();
 
 router.post('/create', async (req, res) => {
@@ -12,6 +12,7 @@ router.post('/create', async (req, res) => {
     if (!instructorId) return res.status(400).send("missing instructorId");
     if (!departmentId) return res.status(400).send("missing departmentId");
 
+
     const data = await createEvent({
         Subject,
         StartTime,
@@ -22,5 +23,29 @@ router.post('/create', async (req, res) => {
     return res.send(data);
 })
 
+
+router.put('/update/:eventId', async (req, res) => {
+    const eventId = req.params.eventId;
+    const { StartTime, EndTime, Subject, instructorId, departmentId } = req.body;
+
+    if (!eventId) return res.status(400).send("missing eventId");
+
+    const payload = {};
+    if (StartTime) payload.StartTime = StartTime;
+    if (EndTime) payload.EndTime = EndTime;
+    if (Subject) payload.Subject = Subject;
+    if (instructorId) payload.instructorId = instructorId;
+    if (departmentId) payload.departmentId = departmentId;
+
+    const data = await updateEvent(eventId, payload);
+    return res.send(data);
+});
+
+router.delete('/delete/:eventId', async (req, res) => {
+    const eventId = req.params.eventId;
+    if (!eventId) return res.status(400).send("missing eventId");
+    const data = await deleteEvent(eventId);
+    return res.send(data);
+})
 
 module.exports = router
--- database/services/events.js
+++ database/services/events.js
@@ -15,4 +15,22 @@ const createEvent = async ({ StartTime, EndTime, Subject, instructorId, departme
   }
 };
 
-module.exports = { createEvent };
+const updateEvent = async (eventId, payload) => {
+  try {
+    const result = await Event.findByIdAndUpdate({ _id: eventId }, payload)
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const deleteEvent = async (eventId) => {
+  try {
+    const result = await Event.deleteOne({ _id: eventId })
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = { createEvent, updateEvent, deleteEvent };
